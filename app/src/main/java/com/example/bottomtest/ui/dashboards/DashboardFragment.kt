5ff6e7d9622a9149a431c42435132d3c747369dc
Evgeniy Malinovskiy <EMalinovskiy@croc.ru>
2022-05-17 01:40:00 +0300

    Two sample dashboards with swithing between them

--- app/src/main/java/com/example/bottomtest/ui/dashboards/DashboardFragment.kt
+++ app/src/main/java/com/example/bottomtest/ui/dashboards/DashboardFragment.kt
@@ -1,13 +1,19 @@
 package com.example.bottomtest.ui.dashboards
 
+import android.R
 import android.os.Bundle
 import android.view.*
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import com.anychart.APIlib
 import com.anychart.AnyChart
 import com.anychart.AnyChartView
 import com.anychart.chart.common.dataentry.DataEntry
 import com.anychart.chart.common.dataentry.ValueDataEntry
+import com.anychart.core.Chart
 import com.example.bottomtest.databinding.FragmentDasboardsBinding
+
 
 class DashboardFragment : Fragment() {
 
@@ -15,6 +21,8 @@ class DashboardFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private val availableCharts = listOf("Dots", "Pie", "Else")
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,20 +33,76 @@ class DashboardFragment : Fragment() {
         _binding = FragmentDasboardsBinding.inflate(inflater, container, false)
         val view = binding.root
         setHasOptionsMenu(true)
+        val anyChartViewDots = binding.anyChartViewDots
+        val anyChartViewPie = binding.anyChartViewPie
+        val spinner = binding.chartTypeSpinner
 
-        val pie = AnyChart.pie()
+        spinner.prompt = "Тип графика"
+        // Создаем адаптер ArrayAdapter с помощью массива строк и стандартной разметки элемета spinner
+        val adapter: ArrayAdapter<String> =
+            ArrayAdapter<String>(requireActivity(), R.layout.simple_spinner_item, availableCharts)
+        // Определяем разметку для использования при выборе элемента
+        adapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
+        // Применяем адаптер к элементу spinner
+        spinner.adapter = adapter
 
-        val data: MutableList<DataEntry> = ArrayList()
-        data.add(ValueDataEntry("John", 10000))
-        data.add(ValueDataEntry("Jake", 12000))
-        data.add(ValueDataEntry("Peter", 18000))
+        APIlib.getInstance().setActiveAnyChartView(anyChartViewDots)
+        anyChartViewDots.setChart(GetDotsChart(anyChartViewDots, anyChartViewPie))
 
-        pie.data(data)
+        APIlib.getInstance().setActiveAnyChartView(anyChartViewPie)
+        anyChartViewPie.setChart(GetPieChart(anyChartViewDots, anyChartViewPie))
 
-        val anyChartView = binding.anyChartView
-        anyChartView.setChart(pie)
+        val itemSelectedListener: AdapterView.OnItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    // Получаем выбранный объект
+                    val item = parent.getItemAtPosition(position) as String
+
+                    val chart: Chart? = when (item) {
+                        "Pie" -> GetPieChart(anyChartViewDots, anyChartViewPie)
+                        "Dots" -> GetDotsChart(anyChartViewDots, anyChartViewPie)
+                        else -> AnyChart.pie()
+                    }
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {}
+            }
+        spinner.onItemSelectedListener = itemSelectedListener
 
         return view
+    }
+
+    private fun GetPieChart(anyChartViewDots: AnyChartView, anyChartViewPie: AnyChartView): Chart? {
+        val data: List<DataEntry> = listOf(ValueDataEntry("John", 10000),
+            ValueDataEntry("Jake", 12000),
+            ValueDataEntry("Peter", 18000)
+        )
+        val chart = AnyChart.pie()
+        chart.data(data)
+        APIlib.getInstance().setActiveAnyChartView(anyChartViewDots)
+        anyChartViewDots.invalidate()
+        APIlib.getInstance().setActiveAnyChartView(anyChartViewPie)
+        anyChartViewPie.bringToFront()
+        return chart;
+    }
+
+    private fun GetDotsChart(anyChartViewDots: AnyChartView, anyChartViewPie: AnyChartView): Chart {
+        val data: List<DataEntry> = listOf(ValueDataEntry("John", 10000),
+            ValueDataEntry("Jake", 12000),
+            ValueDataEntry("Peter", 0)
+        )
+        val chart = AnyChart.column()
+        chart.data(data)
+        APIlib.getInstance().setActiveAnyChartView(anyChartViewPie)
+        anyChartViewPie.invalidate()
+        APIlib.getInstance().setActiveAnyChartView(anyChartViewDots)
+        anyChartViewDots.bringToFront()
+        return chart;
     }
 
     override fun onDestroyView() {
